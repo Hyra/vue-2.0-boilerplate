df8d967d2e6d2490649ef764cd5132742210448f
Hyra <stef@mindthecode.com>
2016-12-23 11:40:47 +0100

    SSR for artists

--- src/views/ArtistsOverview.vue
+++ src/views/ArtistsOverview.vue
@@ -33,7 +33,7 @@ export default {
       myArtists: this.$store.getters.artists
     }
   },
-  // preFetch: fetchPosts,
+  preFetch: fetchPosts,
   beforeMount () {
     fetchPosts(this.$store)
   }
